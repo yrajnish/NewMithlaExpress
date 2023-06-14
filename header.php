ac6467ec2b1b48e7c5da0399a0e807b1034b9e3f
Rajnish yadav <yrajnish4@gmail.com>
2023-06-14 17:24:14 +0545

    deleted unwanted codes

--- header.php
+++ header.php
@@ -196,42 +196,6 @@ window.onclick = function(event) {
   <a href="http://localhost/NewMithlaExpress/category.php?cid=33">Health</a>
   <a href="http://localhost/NewMithlaExpress/category.php?cid=32">Political</a>
   <a href="http://localhost/newmithlaexpress/admin/">Login</a>
-  <!--animation login -->
-  
- <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-  </a>
- <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;" href="http://localhost/newmithlaexpress/admin/">Login</button>
-</div>
-<nav>
-<div id="id01" class="modal">
-  
-  <form class="modal-content animate" action="/action_page.php" method="post">
-    <div class="imgcontainer">
-      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-      <img src="img_avatar2.png" alt="Avatar" class="avatar">
-    </div>
-
-    <div class="container">
-      <label for="uname"><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" required>
-
-      <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
-        
-      <button type="submit">Login</button>
-      <label>
-        <input type="checkbox" checked="checked" name="remember"> Remember me
-      </label>
-    </div>
-
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-      <span class="psw">Forgot <a href="#">password?</a></span>
-    </div>
-  </form>
-</div>
-<!-- /animation login -->
 <!-- /menubar-->
 <!-- Ticker -->
 <div>
